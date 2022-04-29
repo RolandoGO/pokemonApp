4b395cd1944d11bbfa7f975f7654b86b2cc44027
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-04-29 13:46:40 -0300

    first commit, finish Welcome screen, adding general css to index.css, adding google fonts (Radio Canada), adding images in public folder, create the structure of the proyect

---
+++ src/screens/WelcomePage/WelcomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./welcomePage.css"
+
+export default function WelcomePage() {
+  return (
+    <div className='welcomePageImg'>
+
+      <div className='btnContainer'>
+        <button className='loginBtn'>LOGIN</button>
+      </div>
+        
+        
+      
+
+    </div>
+  )
+}
