d013b573ee0ed4d30166af4ff26ba48f9a72e611
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-04-29 17:54:05 -0300

    adding the routes, and the necesary components for it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Routes, Route  } from "react-router-dom";
 import WelcomePage from "./screens/WelcomePage/WelcomePage"
+import Login from "./screens/Login/Login";
+import HomePage from "./screens/HomePage/HomePage"
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <WelcomePage/>
+      
+      <Routes>
+        <Route path="/" element ={<WelcomePage/>}/>
+        <Route path="/login" element ={<Login/>}/>
+        <Route path="/HomePage" element ={<HomePage/>}/>
+        <Route path="*" element={<NotFound/>}/>
+
+
+
+      </Routes>
     </div>
   );
 }
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function NotFound() {
+  return (
+    <div>
+        <h1>NOT FOUND</h1>
+        
+        <Link to="/">Return to previus Page</Link >
+    </div>
+  )
+}
