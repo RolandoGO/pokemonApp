1e34d8b8509a6061d6cbeeb53dc29648ade1d7f0
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-05-01 01:57:44 -0300

    adding css to login page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route  } from "react-router-dom";
 import WelcomePage from "./screens/WelcomePage/WelcomePage"
-import Login from "./screens/Login/Login";
+import Login from "./components/Login/Login";
 import HomePage from "./screens/HomePage/HomePage"
 import NotFound from "./components/NotFound";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
---
+++ src/components/Login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./login.css"
+
+
+export default function Login() {
+  return (
+    <div className='loginWrapper'>
+
+      <div className='loginForm'>
+        <button>Login</button>
+      </div>
+      
+    </div>
+  )
+}
