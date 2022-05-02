c6dd41d86c5c080fc1d037ccf5fe0aed0d92f2b6
Rolando Grethe Ojeda <rolandogretheojeda@gmail.com>
2022-05-02 20:30:17 -0300

    create register component, add the css in register css, add image in public, and create registerService file

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import "./register.css"
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className='registerWrapper'>
+      
+      <form className='registerForm'>
+
+        <input name={"email"} type={"email"} placeholder={"User email:"} required></input>
+        <input name={"password"} type={"password"} placeholder={"5 numeric characters"} required></input>
+
+        <button>Register</button>
+
+
+      </form>
+    
+      
+      </div>
   )
 }
